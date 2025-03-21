6d4f8e9d88d21b5e51b79eacac6de3439e2cafcf
dima1310 <dimaponomarenko13101981@gmail.com>
2025-03-21 11:38:18 +0200

    up

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -81,17 +81,41 @@
 // Окрім цього, підрахуйте суму всіх парних чисел в цьому циклі
 // і поверніть її з функції
 
-function getNumbers(min, max) {
-  let sumEven = 0;
-  for (let i = max; i >= min; i += 1) {
-    console.log(i);
-    if (i % 2 === 0) {
-      sumEven += i;
-    }
-  }
-  console.log(sumEven);
-}
+// function getNumbers(min, max) {
+//   let sumEven = 0;
+
+//   for (let i = max; i >= min; i--) {
+//     console.log(i);
+//     if (i % 2 === 0) {
+//       sumEven += i;
+//     }
+//   }
+
+//   return sumEven;
+// }
+
+// // Приклад виклику функції
+// const result = getNumbers(5, 15);
+// console.log("Сума парних чисел:", result);
+
 // Напишіть функцію fizzBuzz(num), яка приймає число і перевіряє кожне число від 1 до num:
 // Якщо число ділитися  без остачі на 3 - виводить в консоль 'fizz',
 // якщо ділиться  без остачі на 5 - виводить в консоль 'buzz',
 // якщо ділиться  без остачі і на 3, і на 5 - виводить в консоль 'fizzbuzz'.
+
+function fizzBuzz(num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// Приклад виклику функції
+fizzBuzz(20);
